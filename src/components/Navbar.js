ef85383107fe68e72cb3084a2b8f596cc1cb1819
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-15 23:20:07 -0500

    Nav Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
+import logo from "../images/logo.svg";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+          <img src={logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
