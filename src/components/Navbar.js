3f33e6198980b3bc29046b1586e38a66e91d7343
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-16 23:10:21 -0500

    Social Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import logo from "../images/logo.svg";
-import { pageLinks } from "../data";
+import { pageLinks, socialLinks } from "../data";
 
 const Navbar = () => {
   return (
@@ -25,33 +25,21 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li id={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
